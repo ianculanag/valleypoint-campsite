e9396f2dc834894b49833baf6b0f2be2894a4617
Ian Culanag <2160301@slu.edu.ph>
2019-02-21 15:44:12 +0800

    First Staff Controller

---
+++ database/migrations/2019_02_21_072241_create_staff_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStaffTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('staff', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('username')->unique();
+            $table->string('password');
+            $table->string('lastName');
+            $table->string('firstName');
+            $table->enum('role',['admin', 'general', 'lodging', 'cashier']);
+            $table->string('contactNumber');
+            $table->string('emails');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('staff');
+    }
+}
