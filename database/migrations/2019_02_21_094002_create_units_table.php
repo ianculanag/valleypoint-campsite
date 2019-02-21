56f6fb5279fba7811fc8f27d1eb0a50e79688f40
vincetagudar <2168271@slu.edu.ph>
2019-02-21 17:54:47 +0800

    Added unit migration
    Co-authored by: ervszz <2166348@slu.edu.ph>

---
+++ database/migrations/2019_02_21_094002_create_units_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUnitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('units', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('unitNumber');
+            $table->enum('unitType',['room','bed','tent']);
+            $table->integer('capacity');
+            $table->enum('status',['available','reserved','occupied']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('units');
+    }
+}
