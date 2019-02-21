be68beeae15ba05a0c5b95284fd683cc0f8c105d
2143889 <2143889@slu.edu.ph>
2019-02-21 17:12:08 +0800

    Added Guests Migration

---
+++ database/migrations/2019_02_21_090029_create_guests_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGuestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('guests', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('lastName',30);
+            $table->string('firstName',30);
+            $table->string('contactNumber',11);
+            $table->integer('numberOfPax')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('guests');
+    }
+}
