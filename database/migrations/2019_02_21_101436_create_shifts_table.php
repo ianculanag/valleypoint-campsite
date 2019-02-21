01e0f21dd5a7f099d80602711d13f928a814f5cd
Ian Culanag <2160301@slu.edu.ph>
2019-02-21 18:21:38 +0800

    Added shifts migration

---
+++ database/migrations/2019_02_21_101436_create_shifts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateShiftsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shifts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->datetime('shiftStart');
+            $table->datetime('shiftEnd');
+            $table->double('cashStart');
+            $table->integer('staffID')->unsigned();
+            $table->foreign('staffID')->references('id')->on('Staff');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shifts');
+    }
+}
