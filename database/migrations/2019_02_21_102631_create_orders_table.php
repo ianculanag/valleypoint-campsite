6979b3663bd69fa0f777f3e19ed7b7337b4a92a5
dawncundangan <2161487@slu.edu.ph>
2019-02-21 18:27:34 +0800

    Added orders migration

---
+++ database/migrations/2019_02_21_102631_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('orderNumber');
+            $table->enum('paymentStatus',['pending','paid']);
+            $table->dateTime('orderDatetime');
+            $table->integer('productID')->unsigned();
+            $table->integer('shiftID')->unsigned();
+            $table->foreign('productID')->references('id')->on('Products');
+            $table->foreign('shiftID')->references('id')->on('Shifts');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
