6b8c5aafbc64684647f1ec1bea7efc272cf04c93
dawncundangan <2161487@slu.edu.ph>
2019-02-21 18:08:13 +0800

    Added accommodation migration

---
+++ database/migrations/2019_02_21_095518_create_accommodations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAccommodationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accommodations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->enum('accommodationType',['transient','backpacker','glamping']);
+            $table->double('price');
+            $table->enum('paymentStatus',['pending','paid']);
+            $table->integer('staffID')->unsigned();
+            $table->integer('unitID')->unsigned();
+            $table->foreign('staffID')->references('id')->on('Staff');
+            $table->foreign('unitID')->references('id')->on('Units');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accommodations');
+    }
+}
