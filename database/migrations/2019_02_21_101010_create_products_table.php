07d7cfae50c3348b3a471a358b036dc47915500a
2161284 <2161284@slu.edu.ph>
2019-02-21 18:13:24 +0800

    Added products migration

---
+++ database/migrations/2019_02_21_101010_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('productName');
+            $table->enum('productType', ['food', 'beverage']);
+            $table->double('price', 8, 2)->default(00.00);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
