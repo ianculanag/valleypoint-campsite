eb1ddddb462369dbc25af96bd780c8a62b621d3c
2161284 <2161284@slu.edu.ph>
2019-02-27 10:51:08 +0800

    Updated sidebar.html

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,39 +1,40 @@
-<?php
 
-namespace App\Http\Controllers\Auth;
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+            
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+            <title>Log In</title>
+    </head>
 
-use App\Http\Controllers\Controller;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
-
-class LoginController extends Controller
-{
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
-
-    use AuthenticatesUsers;
-
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/home';
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('guest')->except('logout');
-    }
-}
+    <body>
+        <div class="container center-block">
+            <div class="py-5 text-center">
+                <img class="d-block mx-auto mb-4" alt="" width="72" height="72">
+                    <h2>Log in</h2>
+            </div>
+        </div>
+            
+        <div class="container center-block">
+            <form>
+               <center> <div class="mb-3 col-md-4">
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="inputUsername" placeholder="Username">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control" id="inputPassword" placeholder="Password"><br>
+                     <div class="form-group">
+                          <a href="forgetPass.html" class="forgetPass"><p>Forgot Password?</p>
+                      </a></div>
+                    <a href="dashboard.php" button type="submit" class="btn btn-primary">Log in</button></a>
+            </div>
+                </center>   
+                </div>
+            </form>
+        </div>   
+    </body>
+</html>
