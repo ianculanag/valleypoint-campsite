55479ff0d2b58e9c88e8339e33d10992823aaaa3
ervszz <2166348@slu.edu.ph>
2019-03-04 18:44:57 +0800

    added names to each input type

--- resources/views/lodging/addreserve.blade.php
+++ resources/views/lodging/addreserve.blade.php
@@ -13,8 +13,6 @@
                 <form  method="POST" action="/addReservation" class="form-inlin justify-content-center">
                     @csrf
                 <input type="hidden" name="_token" id="token" value="{{ csrf_token() }}">
-
-                
                     <!--div class="form-group">
                         <input type="text" required="required" class="form-control" id="inputGuestid" placeholder="GuestID">
                     </div-->
@@ -28,45 +26,58 @@
                                 <input type="text" name="firstName" required="required" class="form-control" id="inputFirstname" placeholder="First name">
                         </div>
                         <div class="form-group col-md-6">
-                                <input type="text" required="required" class="form-control" id="inputLastname" placeholder="Last name">
+                                <input type="text" name="lastName" required="required" class="form-control" id="inputLastname" placeholder="Last name">
                             </div>  
                     </div>
                       <div class="form-group">
-                        <input type="text" required="required" class="form-control" id="inputContactnum" placeholder="Contact Number">
+                        <input type="text" name="contactNumber" required="required" class="form-control" id="inputContactnum" placeholder="Contact Number">
                     </div>
                     <div class="form-group">
-                        <input type="email" required="required" class="form-control" id="inputEmail" placeholder="Email">
+                        <input type="email"  name ="email" required="required" class="form-control" id="inputEmail" placeholder="Email">
                     </div>
 
                     <div class="row">
                         <div class="form-group col-md-6">
-                            <input type="number" required="required" class="form-control" id="noofpax" placeholder="No. of Pax">
+                            <input type="number" name ="numberOfPax" required="required" class="form-control" id="noofpax" placeholder="No. of Pax">
                         </div>
                     </div>
-                    <div class="form-group">
+
+                    <!--div class="form-group">
                         <select class="custom-select d-block w-100" id="state" required="required">
                             <option value="">Type of Reservation:</option>
                             <option>Glamping</option>
                             <option>Transient</option>
                             <option>Backpacker</option>
                         </select>
-                    </div> 
+                    </div--> 
 
                     <div class="form-group col-md-6">
                         <label for="arrival">Arrival Date & Time:</label>
                     </div>
 
-                    
 
+            <div class="row">
+                    <div class="form-group col-md-6">
+                        <input type="date" name="checkinDate" required="required" class="form-control" id="date">
+                    </div>
+                    <div class="form-group col-md-6">
+                        <input type="time" name="checkinTime" required="required" class="form-control" id="time">
+                    </div>
+                </div>
 
                     <div class="form-group col-md-5">
                         <label for="arrival">Departure Date:</label>
                     </div>
 
+                    <div class="row">
+                            <div class="form-group col-md-6">
+                                <input type="date" name="checkoutDate" required="required" class="form-control" id="date">
+                            </div>
+                            <div class="form-group col-md-6">
+                                <input type="time" name="checkoutTime" required="required" class="form-control" id="time">
+                            </div>
+                        </div>
 
-
-
-                    
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
             </div>
