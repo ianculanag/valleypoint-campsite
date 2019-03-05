a14c6769faf5cfdfb76203d2b5904dc79175da7d
Ian Culanag <2160301@slu.edu.ph>
2019-03-05 16:16:03 +0800

    Populated units db
    Co-authored-by: vincetagudar <2168271@slu.edu.ph>

--- database/migrations/2019_03_05_072504_create_services_table.php
+++ database/migrations/2019_03_05_072504_create_services_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use App\Services;
 
 class CreateServicesTable extends Migration
 {
@@ -20,33 +21,40 @@ class CreateServicesTable extends Migration
             $table->timestamps();
         });
 
-        // Insert the services
-        DB::table('services')->insert(
-            array(
-                'serviceName' => 'Glamping Solo',
-                'price' => '1350',
-            ),
-            array(
-                'serviceName' => 'Glamping 2 pax',
-                'price' => '2500',
-            ),
-            array(
-                'serviceName' => 'Glamping 3 pax',
-                'price' => '3000',
-            ),
-            array(
-                'serviceName' => 'Glamping 4 pax',
-                'price' => '3400',
-            ),
-            array(
-                'serviceName' => 'Transient',
-                'price' => '750',
-            ),
-            array(
-                'serviceName' => 'Backpacker',
-                'price' => '750',
-            )
-        );
+        $service1 = new Services;
+        $service1->serviceName = 'Glamping Solo';
+        $service1->price = '1350';
+        $service1->save();
+
+        $service2 = new Services;
+        $service2->serviceName = 'Glamping 2 pax';
+        $service2->price = '2500';
+        $service2->save();
+
+        $service3 = new Services;
+        $service3->serviceName = 'Glamping 3 pax';
+        $service3->price = '3000';
+        $service3->save();
+
+        $service4 = new Services;
+        $service4->serviceName = 'Glamping 4 pax';
+        $service4->price = '3400';
+        $service4->save();
+
+        $service5 = new Services;
+        $service5->serviceName = 'Backpacker';
+        $service5->price = '750';
+        $service5->save();
+
+        $service6 = new Services;
+        $service6->serviceName = 'Transient';
+        $service6->price = '750';
+        $service6->save();
+
+        $service7 = new Services;
+        $service7->serviceName = 'Airsoft';
+        $service7->price = '750';
+        $service7->save();
     }
 
     /**
