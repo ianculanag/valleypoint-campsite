7a2bdde1086ee06a9b0619f68c2d5054d18fee5c
jmcamille <2161284@slu.edu.ph>
2019-03-11 20:44:04 +0800

    String limit restrict a string to a certain length in viewing guests

--- resources/views/lodging/viewguests.blade.php
+++ resources/views/lodging/viewguests.blade.php
@@ -1,19 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="py-4 text-center">
-  <h3 class="text-center">Guests</h3>
-</div>
-<div class="col-md-12">
-  <table class="table table-striped table-sm reservationTable">
+      <div class="py-4 text-center">
+           <h3 class="text-center">Guests</h3>
+      </div>
+      <div class="col-md-12">
+          <table class="table table-striped table-sm guestsTable">
                 <thead>
                   <tr class="">
-                    <th scope="col" class="text-center">ID</th>
+                    <th scope="col" class="text-center">Guest ID</th>
                     <th scope="col" class="text-center">Type</th>
                     <th scope="col" class="text-center">First Name</th>
                     <th scope="col" class="text-center">Last Name</th>
                     <th scope="col" class="text-center">Listed Under</th>
-                    <th scope="col" class="text-center">No. Of Pax</th>
                     <th scope="col" class="text-center">Contact Number</th>
                     <th scope="col" class="text-center">Action</th>
                   </tr>
@@ -24,33 +23,17 @@
                   @foreach($guest as $guest)
                   <tr class="">
                     <td class="text-center">{{$guest->guestID}}</td>
-                    <td class="text-center">{{$guest->lastName}}</td>
-                    <td class="text-center">{{$guest->firstName}}</td>
+                    <td class="text-center">{{$guest->accommodationID}}</td>
+                    <td class="text-center">{{str_limit($guest->firstName, $limit = 10, $end = '...')}}</td>
+                    <td class="text-center">{{str_limit($guest->lastName, $limit = 10, $end = '...')}}</td>
                     <td class="text-center">{{$guest->listedUnder}}</td>
                     <td class="text-center">{{$guest->contactNumber}}</td>
 
-                    <td>
-                        <button class="btn btn-info" class="text-center">Edit</button>
+                    <td class="text-center">
+                        <button class="btn btn-info">Edit</button>
                     </td>
                   </tr>
                   @endforeach
                   @endif
-{{-- 
-                  <tr class="">
-                    <td class="text-center"></td>
-                    <td class="text-center"></td>
-                    <td class="text-center"></td>
-                    <td class="text-center"></td>
-                    <td><button class="btn btn-success">Accept</button>
-                      <button class="btn btn-warning">Reject</button>
-                      <button class="btn btn-info">Edit</button>
-                      <button class="btn btn-danger">Delete</button>
-                  </td>
-                </tr>                               
-              </tbody> --}}
-
             </table>
-    </div>
-</div>
-
-@endsection
+ @endsection
