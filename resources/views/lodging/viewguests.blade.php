8a1d3ea7bbacee6dbf9df79568ee37c274627b33
2168760 <2168760@slu.edu.ph>
2019-03-11 17:03:33 +0800

    Modified User Interface of view guest

--- resources/views/lodging/viewguests.blade.php
+++ resources/views/lodging/viewguests.blade.php
@@ -1,19 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-
+<div class="py-4 text-center">
+  <h3 class="text-center">Guests</h3>
+</div>
 <div class="table-responsive">
     <div class="container-fluid">
-    <h1 class="display-4">Valleypoint Campsite</h1>
-        <h4 class="text-muted">View Guests</h4>
             <table class="table table-bordered" >
                 <thead>
                   <tr class="">
-                    <th scope="col" class="text-center">Guests ID</th>
-                    <th scope="col" class="text-center">Last Name</th>
+                    <th scope="col" class="text-center">ID</th>
+                    <th scope="col" class="text-center">Type</th>
                     <th scope="col" class="text-center">First Name</th>
+                    <th scope="col" class="text-center">Last Name</th>
                     <th scope="col" class="text-center">Listed Under</th>
+                    <th scope="col" class="text-center">No. Of Pax</th>
                     <th scope="col" class="text-center">Contact Number</th>
+                    <th scope="col" class="text-center">Action</th>
                   </tr>
                 </thead>
                 <tbody>
@@ -27,10 +30,8 @@
                     <td class="text-center">{{$guest->listedUnder}}</td>
                     <td class="text-center">{{$guest->contactNumber}}</td>
 
-                    <td><button class="btn btn-success">Accept</button>
-                        <button class="btn btn-warning">Reject</button>
-                        <button class="btn btn-info">Edit</button>
-                        <button class="btn btn-danger">Delete</button>
+                    <td>
+                        <button class="btn btn-info" class="text-center">Edit</button>
                     </td>
                   </tr>
                   @endforeach
