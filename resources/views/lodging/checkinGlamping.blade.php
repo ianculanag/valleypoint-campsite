fb530a8cfac9ffcb94ecc789b48ebcec67d87175
vincetagudar <2168271@slu.edu.ph>
2019-03-12 19:29:48 +0800

    Initial Form storing data when fail/error (fixed)

--- resources/views/lodging/checkinGlamping.blade.php
+++ resources/views/lodging/checkinGlamping.blade.php
@@ -76,19 +76,11 @@
             <div class="row">
                 <div class="form-group col-md-6">
                 <label for="arrivalDate">Arrival Date:</label>
-<<<<<<< HEAD
-                    <input type="date" name="checkinDate" required="required" class="form-control" id="datePicker" value="<?php echo date("Y-m-d");?>">
+                <input type="date" name="checkoutDate" required="required" class="form-control" id="date" value="{{old('checkinDate')}}">
                 </div>
                 <div class="form-group col-md-6">
                 <label for="arrivalTime">Time: </label>
-                    <input type="time" name="checkinTime" required="required" class="form-control" id="time" value="14:00">
-=======
-                    <input type="date" name="checkinDate" required="required" class="form-control" id="date" value="{{old('checkinDate')}}">
-                </div>
-                <div class="form-group col-md-6">
-                <label for="arrivalTime">Time: </label>
-                    <input type="time" name="checkinTime" required="required" class="form-control" id="time" value="{{old('checkinTime')}}">
->>>>>>> Initial form storing data when error/fail
+                <input type="time" name="checkinTime" required="required" class="form-control" id="time" value="14:00{{old('checkinTime')}}">
                 </div>
             </div>
             
@@ -99,39 +91,9 @@
                 </div>
                 <div class="form-group col-md-6">
                     <label for="departureTime">Time:</label>
-<<<<<<< HEAD
-                    <input type="time" name="checkoutTime" required="required" class="form-control" id="time" value="12:00">
-=======
-                    <input type="time" name="checkoutTime" required="required" class="form-control" id="time" value="{{old('checkoutTime')}}">
->>>>>>> Initial form storing data when error/fail
+                <input type="time" name="checkoutTime" required="required" class="form-control" id="time" value="12:00{{old('checkoutTime')}}">
                 </div>
             </div>
-
-            <div class="row">
-                <div class="card p-2 col-md-11 ">
-                <label for="payment">Payment</label>
-                <div class="row">
-                    <div class="form-group col-md-6">
-                    <label for="amount">Amount:</label>
-                        <div class="input-group">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text">₱</span>
-                            </div>                    
-                        <input type="number" name="amountPaid" placeholder="0" class="form-control" id="amount">
-                    </div>
-                    </div>
-    
-                    
-                    <div class="form-group col-md-6">
-                    <label for="arrivalTime">Status: </label>
-                    <select class="form-control" id="status" name="paymentStatus">
-                        <option value="paid">Fully paid</option>
-                        <option value="pending">Pending</option>
-                    </select>
-                    </div>
-                    </div>
-                </div>
-                </div>
 
             <button type="submit" value="Submit" style="width:10em;" class="btn btn-info float-right mt-5" data-toggle="modal" data-target="#check-in guests">
                     Check-in
