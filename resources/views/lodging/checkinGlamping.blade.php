c3748876d2380547be8a7c33f690999dbde53bd6
dawncundangan <2161487@slu.edu.ph>
2019-03-18 16:06:35 +0800

    Updated check-in glamping page

--- resources/views/lodging/checkinGlamping.blade.php
+++ resources/views/lodging/checkinGlamping.blade.php
@@ -1,7 +1,194 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+{{--@foreach ($guest as $guestDetails)--}}
+        <div class="py-5 text-center">
+            <a href="/glamping">
+                <span style="float:left;">
+                    <i class="fa fa-chevron-left" aria-hidden="true"></i>
+                    <strong>Back</strong>
+                </span>
+            </a>
+            <h3>Check-in Form</h3>
+        </div>
+        <div class="row">
+            <div class="col-md-4 order-md-2 mb-4">
+                <form class="card p-2">
+                    <h4 class="text-muted" style="text-align:center; padding:0.5em;">Invoice</h4>
+                    <table class="table table-striped" style="font-size:.83em;">
+                        <thead>
+                            <tr>
+                                <th scope="col" style="width:55%">Desciption</th>
+                                <th scope="col">Qty.</th>
+                                <th scope="col">Price</th>
+                                <th scope="col">Total</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {{--@php
+                                $total = 0;
+                            @endphp
+                            @foreach($charges as $charge)--}}
+                            <tr>
+                                <td>{{--$charge->serviceName--}}</td>
+                                <td style="text-align:right;">{{--$charge->numberOfPax--}}</td>
+                                <td style="text-align:right;">{{--$charge->price--}}</td>
+                                <td style="text-align:right;">{{--($charge->totalPrice)--}}</td>
+                            </tr>
+                            {{--@php
+                                $total += $charge->totalPrice;
+                            @endphp
+                            @endforeach --}}
+                            <tr>
+                                <th colspan="3" scope="row">TOTAL:</th>
+                                <th style="text-align:right;">{{--$total--}}</th>
+                            </tr>
+                        </tbody>
+                    </table>
+                </form>
+            </div>
+            <div class="col-md-8 order-md-1 check-out-form">
+                <form method="POST" action="/updateDetails">
+                    {{--@csrf--}}
+                    <input type="hidden" name="_token" id="token" value="{{-- csrf_token() --}}">
+                    <h5 style="margin-bottom:.80em;">Guest Details</h5>
+                    <div class="form-group row">
+                        <div class="col-md-5 mb-1">
+                            <label for="firstName">First name</label>
+                            <input class="form-control" type="text" name="firstName" maxlength="15" placeholder="" value="">
+                        </div>
+                        <div class="col-md-7 mb-1">
+                            <label for="lastName">Last name</label>
+                            <input class="form-control" type="text" name="lastName"  maxlength="20" placeholder="" value="">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-md-5 mb-1">
+                            <label for="contactNumber">Contact number</label>
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">
+                                        <i class="fa fa-phone" aria-hidden="true"></i>
+                                    </span>
+                                </div>
+                                <input class="form-control" type="text" name="contactNumber" maxlength="11" placeholder="" value="">
+                            </div>
+                        </div>
+                        <div class="col-md-3 mb-1">
+                            <label for="numberOfPax">No. of pax</label>
+                            <input class="form-control" type="number" name="numberOfPax" placeholder="" value="" min="1" max="10">
+                        </div>
+                        <div class="col-md-3 mb-1" style="display:none; position:absolute;">
+                            <label for="numberOfPax">No. of pax</label>
+                            <input class="form-control"  type="number" name="numberOfPax" placeholder="" value="" min="1" max="10">
+                        </div>
+                        <div class="col-md-4 mb-1 form-group">
+                            <label for="accommodationType">Accommodation</label>
+                            <select name="serviceName" class="form-control">
+                                <option>Glamping</option>
+                                <option>Backpacker</option>
+                            </select>
+                        </div>
+                    </div>
+                    <hr class="mb-4">
+                    <h5 style="margin-bottom:.80em;">Unit Details</h5>
+                    <div class="form-group row">
+                        <div class="col-md-2 mb-1">
+                            <label for="unitID">No. of units</label>
+                            <input class="form-control" type="number" name="numberOfUnits" placeholder="" value="" min="1" max="10">
+                        </div>
+                        <div class="col-md-10 mb-1">
+                            <label for="unitNumber">Unit/s</label>
+                            <input class="form-control" type="text" name="unitNumbers" placeholder="This will be a listbox/tokenfield" role="listbox" value="">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-md-6 mb-1">
+                            <label for="checkinDate">Check-in date</label>
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">
+                                        <i class="fa fa-calendar" aria-hidden="true"></i>
+                                    </span>
+                                </div>
+                                <input type="date" name="checkinDate" required="required" class="form-control" id="date" value="">
+                            </div>
+                        </div>
+                        <div class="col-md-6 mb-1">
+                            <label for="checkoutDate">Departure date</label>
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">
+                                        <i class="fa fa-calendar" aria-hidden="true"></i>
+                                    </span>
+                                </div>
+                                <input type="date" name="checkoutDate" required="required" class="form-control" id="date" value="">
+                            </div>
+                        </div>
+                    </div>
+
+                    <hr class="mb-4">
+                    <div class="form-group row pb-3">
+                        <div class="col-md-12 mb-1">
+                            <h5 style="margin-bottom:.80em;">Additional Services</h5>
+                        </div>
+                        <input type="hidden" name="additionalServiceAccommodationID" value="">
+                        <div class="col-md-3 mb-1">
+                            <label for="additionalServiceName">Service name</label>
+                            <select name="additionalServiceName" id="serviceSelect" class="form-control serviceSelect">
+                                <option value="" selected disabled >Choose...</option>
+                                <option value="6">Airsoft</option>
+                                <option value="7">Archery</option>
+                            </select>
+                        </div>
+                        <div class="col-md-2 mb-1">
+                            <label for="additionalServiceNumberOfPax">Pax</label>
+                            <input class="form-control paxSelect" type="number" id="additionalServiceNumberOfPax" name="additionalServiceNumberOfPax" placeholder="" value="" min="1" max="10">
+                        </div>
+                        <div class="col-md-3 mb-1">
+                            <label for="additionalServiceUnitPrice">Unit price</label>
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">₱</span>
+                                </div>
+                                <input class="form-control" type="text" id="additionalServiceUnitPrice" name="additionalServiceUnitPrice" placeholder="" value="" disabled>
+                            </div>
+                        </div>
+                        <div class="col-md-3 mb-1">
+                            <label for="additionalServiceTotalPrice">Total price</label>
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">₱</span>
+                                </div>
+                                <input class="form-control" type="text" id="additionalServiceTotalPrice" name="additionalServiceTotalPrice" placeholder="" value="" disabled>
+                            </div>
+                        </div>
+
+                        <div style="margin-top:2em;">
+                            <div class="input-group">
+                                <button type="submit" class="btn btn-primary additionalServiceForm">
+                                    <span class="fa fa-plus" aria-hidden="true"></span>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <input class="form-control" type="number" name="numberOfAdditionalCharges" value="1" style="display:none; position:absolute;">
+                    <input class="form-control" type="text" name="serviceID1" value="6" style="display:none; position:absolute;">
+                    <input class="form-control" type="number" name="numberOfPaxAdditional1" value="5" style="display:none; position:absolute;">
+                    <input class="form-control" type="text" name="paymentStatus1" value="paid" style="display:none; position:absolute;">
+                    
+                    <div style="float:right;">
+                        <button class="btn btn-success" style="width:10em;" type="submit">Check-in</button>
+                        <a href="/glamping" style="text-decoration:none;">
+                            <button class="btn btn-secondary" style="width:10em;" type="button">Cancel</button>
+                        </a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    {{--@endforeach--}}
+{{-- <div class="container">
     <div class="py-5 text-center">
             <a href="/glamping">
                 <span style="float:left;">
@@ -76,7 +263,7 @@
             <div class="row">
                 <div class="form-group col-md-6">
                 <label for="arrivalDate">Arrival Date:</label>
-                <input type="date" name="checkinDate" required="required" class="form-control" id="date" value="<?php echo date("Y-m-d");?>">
+                <input type="date" name="checkinDate" required="required" class="form-control" id="date" value="<!--?php echo date("Y-m-d");?>">
                 </div>
                 <div class="form-group col-md-6">
                 <label for="arrivalTime">Time: </label>
@@ -135,7 +322,7 @@
             <input type="text" name="firstName2" id="token" value="Albren" style="display:none;">
             <input type="text" name="lastName2" id="token" value="Cundangan Jr." style="display:none;">
             <input type="text" name="contactNumber2" id="token" value="09078218097" style="display:none;">
-            {{DO NOT TOUCH --}}
+            {{DO NOT TOUCH }}
             </div>        
         </form>
     </div>
@@ -165,5 +352,5 @@
         <input type="hidden" name="contactNumber2" id="token" value="09083019923"-->
         </div> 
     </div>
-</div>
+</div> --}}
 @endsection
