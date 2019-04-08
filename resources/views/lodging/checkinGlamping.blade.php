686ee4eb67d82a1d43e3b3ba8ecb217e2fae2d5d
dawncundangan <2161487@slu.edu.ph>
2019-04-08 14:22:48 +0800

    Fixed check-in glamping error

--- resources/views/lodging/checkinGlamping.blade.php
+++ resources/views/lodging/checkinGlamping.blade.php
@@ -13,7 +13,7 @@
             </a>
             <h3>Check-in Form</h3>
         </div>   
-        <form method="POST" action="/checkinGlamping">
+        <form method="POST" action="/checkin-glamping">
         @csrf
         <input type="hidden" name="_token" id="token" value="{{ csrf_token() }}">
         <input type="hidden" name="selectedUnit" id="selectedUnit" value="{{$unit->unitNumber}}">
