df86bf732696cee0d6e559369bb80e8cc9a8aa5f
vincetagudar <2168271@slu.edu.ph>
2019-05-02 16:37:05 +0800

    Added sample data to sales report

--- resources/views/restaurant/salesreport.blade.php
+++ resources/views/restaurant/salesreport.blade.php
@@ -1,8 +1,7 @@
 @extends('layouts.app')
-
 @section('content')
 <div class="container pb-5">
-<div class="pt-3 pb-3">
+<div class="pt-3 pb-6">
         <a href="">
             <span style="float:left;">
                 <i class="fa fa-chevron-left" aria-hidden="true"></i>
@@ -11,7 +10,7 @@
         </a>
         <h3 class="text-center">Sales Report</h3>
     </div>
-    <div class="col-md-12">
+    <div class="col-md-12 text-center">
         <table id="guestsTable" data-order='[[ 0, "asc" ]]' class="table table-sm dataTable stripe compact" cellspacing="0">
           <thead>
             <tr class="">
@@ -19,10 +18,13 @@
               <th>Item Price</th>
               <th>Quantity Sold</th>
               <th>Total Price</th>
-              
             </tr>
           </thead>
           <tbody>
+              <th>Tapsilog</th>
+              <th>Php 70.00</th>
+              <th>3</th>
+              <th>Php 210.00</th>
           </tbody>
     </table>
 </div>
