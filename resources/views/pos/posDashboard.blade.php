118bf625c84db1b57ae5b31a5852743e7cb16e3c
ervszz <2166348@slu.edu.ph>
2019-05-03 10:00:20 +0800

    Added the pos.html contents to posDashboard.blade(Still has layout issues)

--- resources/views/pos/posDashboard.blade.php
+++ resources/views/pos/posDashboard.blade.php
@@ -7,8 +7,132 @@
         <div class="container lodging-tabs">
             <ul class="nav nav-tabs pt-0" style="width:93%">
                 <li class="nav-item">
-                    <a class="nav-link active" href="/transient-backpacker">Point of Sales</a>
+                    <a class="nav-link active" href="">New Orders</a>
+                    <li class="nav-item">
+                        <a class="nav-link" style="color:#505050"; href=""">View Sales</a>
+                    </li>
                 </li>
             </ul>
         </div>
+        <div class="container-center">
+            <div class="row">
+                <div class="card" style="width: 16rem; height: 8em; margin: 1em; margin-left: 5em;">
+                    <div class="card-body">
+                        <h5 class="card-title">Table 1
+                            <span class="badge badge-success float-right" style="font-size:.55em;">Available</span>
+                        </h5>
+                        <p class="card-text">Status: </p>
+                        <p class="card-text">Amount: </p>
+                     </div>
+                </div>  
+
+                <div class="card" style="width: 16rem; height: 8em; margin: 1em">
+                    <div class="card-body">
+                        <h5 class="card-title">Table 2
+                            <span class="badge badge-success float-right" style="font-size:.55em;">Available</span>
+                        </h5>
+                        <p class="card-text">Status: </p>
+                        <p class="card-text">Amount: </p>
+                    </div>
+                </div>   
+
+                <div class="card" style="width: 16rem; height: 8em; margin: 1em">
+                    <div class="card-body">
+                        <h5 class="card-title">Table 3
+                            <span class="badge badge-success float-right" style="font-size:.55em;">Available</span>
+                        </h5>
+                        <p class="card-text">Status: </p>
+                        <p class="card-text">Amount: </p>
+                       </div>
+                </div>  
+
+                <div class="card" style="width: 16rem; height: 8em; margin: 1em">
+                    <div class="card-body">
+                        <h5 class="card-title">Table 4
+                            <span class="badge badge-success float-right" style="font-size:.55em;">Available</span>
+                        </h5>
+                        <p class="card-text">Status: </p>
+                        <p class="card-text">Amount: </p>
+                        </div>
+                </div> 
+
+                <div class="card" style="width: 16rem; height: 8em; margin: 1em; margin-left: 5em">
+                    <div class="card-body">
+                        <h5 class="card-title">Table 5
+                            <span class="badge badge-success float-right" style="font-size:.55em;">Available</span>
+                        </h5>
+                        <p class="card-text">Status: </p>
+                        <p class="card-text">Amount: </p>
+                       </div>
+                </div>  
+
+                <div class="card" style="width: 16rem; height: 8em; margin: 1em">
+                    <div class="card-body">
+                        <h5 class="card-title">Table 6
+                            <span class="badge badge-success float-right" style="font-size:.55em;">Available</span>
+                        </h5>
+                        <p class="card-text">Status: </p>
+                        <p class="card-text">Amount: </p>
+                        </div>
+                </div>  
+
+                <div class="card" style="width: 16rem; height: 8em; margin: 1em">
+                    <div class="card-body">
+                        <h5 class="card-title">Table 7 
+                            <span class="badge badge-success float-right" style="font-size:.55em;">Available</span>
+                        </h5>
+                        <p class="card-text">Status: </p>
+                        <p class="card-text">Amount: </p>
+                        </div>
+                </div>  
+
+                <div class="card" style="width: 16rem; height: 8em; margin: 1em">
+                    <div class="card-body">
+                        <h5 class="card-title">Table 8
+                            <span class="badge badge-success float-right" style="font-size:.55em;">Available</span>
+                        </h5>
+                        <p class="card-text">Status: </p>
+                        <p class="card-text">Amount: </p>
+                        </div>
+                </div>  
+
+                <div class="card" style="width: 16rem; height: 8em; margin: 1em; margin-left: 5em">
+                    <div class="card-body">
+                        <h5 class="card-title">Table 9
+                            <span class="badge badge-success float-right" style="font-size:.55em;">Available</span>
+                        </h5>
+                        <p class="card-text">Status: </p>
+                        <p class="card-text">Amount: </p>
+                        </div>
+                </div>
+
+                <div class="card" style="width: 16rem; height: 8em; margin: 1em">
+                    <div class="card-body">
+                        <h5 class="card-title">Table 10
+                            <span class="badge badge-success float-right" style="font-size:.55em;">Available</span>
+                        </h5>
+                        <p class="card-text">Status: </p>
+                        <p class="card-text">Amount: </p>
+                        </div>
+                </div>  
+
+                <div class="card" style="width: 16rem; height: 8em; margin: 1em">
+                    <div class="card-body">
+                        <h5 class="card-title">Table 11
+                            <span class="badge badge-success float-right" style="font-size:.55em;">Available</span>
+                        </h5>
+                        <p class="card-text">Status: </p>
+                        <p class="card-text">Amount: </p>
+                       </div>
+                </div> 
+
+                <div class="card" style="width: 16rem; height: 8em; margin: 1em">
+                    <div class="card-body">
+                        <h5 class="card-title">Table 12
+                            <span class="badge badge-success float-right" style="font-size:.55em;">Available</span>
+                        </h5>
+                        <p class="card-text">Status: </p>
+                        <p class="card-text">Amount: </p>
+                     </div>
+                </div> 
 @endsection
