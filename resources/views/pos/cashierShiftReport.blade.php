5e43b9e6c4160f5367e3638dae353c61b9e6ccba
dawncundangan <2161487@slu.edu.ph>
2019-05-06 16:23:48 +0800

    Added revised cashier shift report page (unfinished)

--- resources/views/pos/cashierShiftReport.blade.php
+++ resources/views/pos/cashierShiftReport.blade.php
@@ -2,88 +2,88 @@
 
 @section('content')
     <div class="container pb-5">
-        <div class="pt-3 pb-3 text-center">
-            <a href="/pos">
-                <span style="float:left;">
-                    <i class="fa fa-chevron-left" aria-hidden="true"></i>
-                    <strong>Back</strong>
-                </span>
-            </a>
-            <h3>Cashier Shift Report</h3>
-        </div>   
-        <form method="POST" action="/checkin-backpacker">
-        @csrf
-        <input type="hidden" name="_token" id="token" value="{{ csrf_token() }}">
-        <!--<input type="hidden" name="selectedUnit" id="selectedUnit" value="">
-        <input type="hidden" name="backpackerQuantity" id="backpackerQuantity" value="">-->
-        <h3>Sales Report</h3>
-        <label><h5>Cashier:</h5></label> Tony Stark 
-        <label><h5>Date:</h5></label> May 1,2019
-        <label><h5>Cash Start:</h5></label> Php.10,000
-        <br><label><h5>Start Period:</h5></label> 8:00am
-        <label><h5>End Period:</h5></label> 5:00pm
-        <div class="row">
-            <div class="col-md-10 order-md-2 mb-4 mx-0">
-                <div class="card p-0 mx-0">
-                    <h4 class="text-muted" style="text-align:center; padding:0.5em;">Sales</h4>
-                    <table class="table table-striped" style="font-size:.88em;">
-                        <thead>
-                            <tr>
-                                <th scope="col">Time</th>
-                                <th scope="col">Product</th>
-                                <th scope="col">Price</th>
-                                <th scope="col">Qty</th>
-                                <th scope="col">Total</th>
-                            </tr>
-                        </thead>
-                        <tbody id="invoiceRows">
-                            <tr id="">
-                                <td style="display:none;"><input id="" class="form-check-input" type="checkbox" checked></td>
-                                <td id="invoiceItem" class="invoiceDescriptions">8:35am</td>
-                                <td id="invoiceItem" class="invoiceDescriptions">Tapsilog</td>
-                                <td id="invoiceQuantity"  class="invoiceQuantities">1</td>
-                                <td id="invoiceUnitPrice"  class="invoiceUnitPrices">100</td>
-                                <td id="invoiceTotalPrice"  class="invoicePrices">100</td>
-                            </tr>
-                            <tr id="">
-                                <td style="display:none;"><input id="" class="form-check-input" type="checkbox" checked></td>
-                                <td id="invoiceItem" class="invoiceDescriptions">8:37am</td>
-                                <td id="invoiceItem" class="invoiceDescriptions">Carbonara</td>
-                                <td id="invoiceQuantity"  class="invoiceQuantities">1</td>
-                                <td id="invoiceUnitPrice" class="invoiceUnitPrices">150</td>
-                                <td id="invoiceTotalPrice" class="invoicePrices">150</td>
-                            </tr>
-                            <tr id="">
-                                <td style="display:none;"><input id="" class="form-check-input" type="checkbox" checked></td>
-                                <td id="invoiceItem" class="invoiceDescriptions">8:45am</td>
-                                <td id="invoiceItem" class="invoiceDescriptions">Nachos</td>
-                                <td id="invoiceQuantity"  class="invoiceQuantities">2</td>
-                                <td id="invoiceUnitPrice" class="invoiceUnitPrices">100</td>
-                                <td id="invoiceTotalPrice" class="invoicePrices">200</td>
-                            </tr>
-                            </tbody>
-                            <tfoot>
-                            <tr>
-                                <th colspan="3" scope="row">TOTAL:Php450</th>
-                                <th id="invoiceGrandTotal" style="text-align:right;"></th>
-                            </tr>
-                            
-                            {{--<tr>
-                                <th colspan="1">Amount Paid:</th>
-                                <th style="text-align:right;"  colspan="3">
-                                <input type="number" name="amountPaid" placeholder="0" min="0" style="text-align:right;" class="form-control" id="amount" required>
-                                </th>
-                            </tr>--}}
-                        </tfoot>
-                    </table>
+            <div class="card col-md-8 offset-md-2 col-sm-12 py-4 ">
+                <div class="row">
+                    <div class="col-md-8 col-sm-8 px-5 pt-3">
+                        <h4 class=""> Cashier Shift Report </h4>
+                        <h6 class="pb-5"> June 24, 2019 </h6>
+                        <div class="form-group row py-0 my-0">
+                            <label for="name" class="col-sm-4 pt-2" style="font-size:0.80em;">Name:</label>
+                            <input class="form-control-plaintext col-sm-8" type="text" name="name" value="Sheila Yu" readonly>
+                        </div>
+                        <div class="form-group row py-0 my-0">
+                            <label for="shiftDuration" class="col-sm-4 pt-2" style="font-size:0.80em;">Shift Duration:</label>
+                            <input class="form-control-plaintext col-sm-8" type="text" name="shiftDuration" value="9:00 AM - 8:00 PM" readonly>
+                        </div>
+                        <div class="form-group row py-0 my-0">
+                            <label for="cashStart" class="col-sm-4 pt-2" style="font-size:0.80em;">Cash Start:</label>
+                            <input class="form-control-plaintext col-sm-8" type="text" name="cashStart" value="₱ 1800.00" readonly>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-3">
+                        <img src={{asset('logo.jpg')}} class="float-right" style="height:7.5em; width:9.75em;" aria-hidden="true"></img>
+                    </div>
                 </div>
-                <label><h5>Tony Stark's Total Sale:</h5></label>    Php450
-                <br><label><h5>Cash close:</h5></label>     Php10,450
-                <br><button type="button" class="btn btn-primary" style="text-align:center;width:10em" id="proceedToPayment" data-toggle="modal" data-target="">
-                    Change Register
-                </button>
-            </div>
-        </div>
+                <div class="card-body">
+                    <div class="px-2">
+                        <table class="table table-sm table-bordered" style="font-size:.90em;">
+                            <tbody>
+                                <tr>
+                                    <td class="text-center" style="width:6%;"> No. </td>
+                                    <td class="text-center"> Description </td>
+                                    <td class="text-center" style="width:7%;"> Qty. </td>
+                                    <td class="text-center" style="width:14%;"> Unit Price </td>
+                                    <td class="text-center" style="width:14%;"> Total Price </td>
+                                    <td class="text-center" style="width:14%;"> Amount Pd. </td>
+                                    <td class="text-center" style="width:14%;"> Change Due </td>
+                                    <td class="text-center"> Payment time </td>
+                                </tr>
+                                <tr>
+                                    <td> 1 </td>
+                                    <td> Tapsilog </td>
+                                    <td> 3 </td>
+                                    <td class="text-right"> ₱ 100.00 </td>
+                                    <td class="text-right"> ₱ 300.00 </td>
+                                    <td class="text-right"> ₱ 300.00 </td>
+                                    <td class="text-right"> ₱ 0.00 </td>
+                                    <td> 9:35 AM </td>
+                                </tr>
+                                <tr>
+                                    <td> 2 </td>
+                                    <td> Carbonara </td>
+                                    <td> 2 </td>
+                                    <td class="text-right"> ₱ 140.00 </td>
+                                    <td class="text-right"> ₱ 280.00 </td>
+                                    <td class="text-right"> ₱ 300.00 </td>
+                                    <td class="text-right"> ₱ 20.00 </td>
+                                    <td> 10:45 AM </td>
+                                </tr>
+                                <tr>
+                                    <td> 3 </td>
+                                    <td> Carbonara </td>
+                                    <td> 1 </td>
+                                    <td class="text-right"> ₱ 140.00 </td>
+                                    <td class="text-right"> ₱ 140.00 </td>
+                                    <td class="text-right"> ₱ 150.00 </td>
+                                    <td class="text-right"> ₱ 10.00 </td>
+                                    <td> 11:30 AM </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="form-group row py-0 my-0 col-md-8">
+                        <label for="cashEnd" class="col-sm-4 pt-2" style="font-size:0.80em;">Cash End:</label>
+                        <input class="form-control-plaintext col-sm-8" type="text" name="cashEnd" value="₱ 2520.00" readonly>
+                    </div>
+                    <div class="form-group row py-0 my-0 col-md-8">
+                        <label for="shiftSales" class="col-sm-4 pt-2" style="font-size:0.80em;">Total Sales: </label>
+                        <input class="form-control-plaintext col-sm-8" type="text" name="name" value="₱ 720.00" readonly>
+                    </div>
+                    <button type="button" class="btn btn-primary float-right mx-3" style="" id="" data-toggle="" data-target="">
+                        Change Register
+                    </button>
+                </div>
+            </div> 
+        </div>      
     </div>
-    
 @endsection
