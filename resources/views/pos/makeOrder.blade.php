be81801760aea3bdd3788d0a50e211dbc4367663
ervszz <2166348@slu.edu.ph>
2019-05-10 10:07:33 +0800

    Converted orderPage.html to blade file

--- resources/views/pos/makeOrder.blade.php
+++ resources/views/pos/makeOrder.blade.php
@@ -1,11 +1,111 @@
 @extends('layouts.app')
 
 @section('content')
-<a href="/pos">
+<a href="/pos-dashboard">
     <span style="float:left;">
         <i class="fa fa-chevron-left" aria-hidden="true"></i>
         <strong>Back</strong>
     </span>
 </a>
-<h1>Create Order Interface</h1>
+        <div class="container pb-5"> 
+            <div class="row">
+                <div class="col-md-4 order-md-8 mb-4 mx-0">
+                    <div class="card p-0 mx-0">
+                        <h4 class="text-muted" style="text-align:center; padding:0.5em;">Charges</h4>
+                        <table class="table table-sm" style="font-size:.88em;">
+                            <thead>
+                                <tr>
+                                    <th scope="col" style="width:40%">Description</th>
+                                    <th scope="col">Qty.</th>
+                                    <th scope="col">Price</th>
+                                    <th scope="col">Total</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Tapsilog</td>
+                                    <td>1</td>
+                                    <td>99.00</td>
+                                    <td>99.00</td>
+                                </tr>
+                                <tr>
+                                    <td>Carbonara</td>
+                                    <td>1</td>
+                                    <td>159.00</td>
+                                    <td>159.00</td>
+                                </tr>
+                                <tr>
+                                    <td>Mango shake</td>
+                                    <td>1</td>
+                                    <td>120.00</td>
+                                    <td>120.00</td>
+                                </tr>
+                            </tbody>
+                            <tfoot>
+                                <tr>
+                                    <th colspan="3" scope="row">TOTAL: 378.00</th>
+                                    <th id="invoiceGrandTotal" style="text-align:right;"></th>
+                                </tr>
+                                <tr>
+                                    <td colspan="4"><button type="button" class="btn btn-primary" style="text-align:center;width:8em">
+                                        Get payment
+                                    </button></td>
+                                </tr>
+                            </tfoot>
+                        </table>
+                    </div>
+                </div>
+        
+                <div class="col-md-8">
+                    <div class="nav nav-tabs" aria-orientation="vertical">
+                        <ul class="nav flex-column">
+                            <li class="nav-item">
+                                <a class="nav-link active" href="">Appetizer</a>
+                                <li class="nav-item">
+                                    <a class="nav-link" style="color:#505050"; href="">Bread</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" style="color:#505050"; href="">Breakfast</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" style="color:#505050"; href="">Group Meals</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" style="color:#505050"; href="">Noodles</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" style="color:#505050"; href="">Rice Bowl</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" style="color:#505050"; href=""> Soup</a>
+                                </li>
+                            </li>
+                        </ul>
+                        <div class="tab-content" id="">
+                            <div class="card" style="width: 7rem; height: 5em; margin-left: 1em;">
+                                <div class="card-body">
+                                    <p class="card-title">Sizzling Pork Sisig</p>
+                                </div>
+                            </div> 
+                            <div class="card" style="width: 7rem; height: 5em; margin-left: 1em;">
+                                <div class="card-body">
+                                    <p class="card-title">Sizzling Tuna</p>
+                                </div>
+                            </div>
+                            <div class="card" style="width: 7rem; height: 5em; margin-left: 1em;">
+                                <div class="card-body">
+                                    <p class="card-title">Nachos</p>
+                                </div>
+                            </div>  
+                            <div class="card" style="width: 7rem; height: 5em; margin-left: 1em;">
+                                <div class="card-body">
+                                    <p class="card-title">Dynamite</p>
+                                </div>
+                            </div>     
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
 @endsection
--- resources/views/pos/posDashboard.blade.php
+++ resources/views/pos/posDashboard.blade.php
@@ -9,133 +9,67 @@
                 <li class="nav-item">
                     <a class="nav-link active" href="">View Tables</a>
                     <li class="nav-item">
-                        <a class="nav-link" style="color:#505050"; href="/MakeOrder">New order</a>
+                        <a class="nav-link" style="color:#505050"; href="/make-order">New order</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" style="color:#505050"; href="/CashierShiftReport">View Sales</a>
+                        <a class="nav-link" style="color:#505050"; href="/cashier-shift-report">View Sales</a>
                     </li>
                 </li>
             </ul>
         </div>
         <div class="container-center">
             <div class="row">
-                <div class="card" style="width: 16rem; height: 8em; margin: 1em; margin-left: 5em; background-image:url({{asset('table.png')}});">
-                    <div class="card-body">
-                        <h5 class="card-title">Table 1
-                            <span class="badge badge-success float-right" style="font-size:.55em;">Available</span>
-                        </h5>
-                        <p class="card-text">Status: </p>
-                        <p class="card-text">Amount: </p>
-                     </div>
-                </div>  
-
-                <div class="card" style="width: 16rem; height: 8em; margin: 1em; background-image:url({{asset('table.png')}})">
-                    <div class="card-body">
-                        <h5 class="card-title">Table 2
-                            <span class="badge badge-success float-right" style="font-size:.55em;">Available</span>
-                        </h5>
-                        <p class="card-text">Status: </p>
-                        <p class="card-text">Amount: </p>
-                    </div>
-                </div>   
-
-                <div class="card" style="width: 16rem; height: 8em; margin: 1em; background-image:url({{asset('table.png')}})">
-                    <div class="card-body">
-                        <h5 class="card-title">Table 3
-                            <span class="badge badge-success float-right" style="font-size:.55em;">Available</span>
-                        </h5>
-                        <p class="card-text">Status: </p>
-                        <p class="card-text">Amount: </p>
-                       </div>
-                </div>  
-
-                <div class="card" style="width: 16rem; height: 8em; margin: 1em; margin-left: 5em; background-image:url({{asset('table.png')}})">
-                    <div class="card-body">
-                        <h5 class="card-title">Table 4
-                            <span class="badge badge-success float-right" style="font-size:.55em;">Available</span>
-                        </h5>
-                        <p class="card-text">Status: </p>
-                        <p class="card-text">Amount: </p>
-                        </div>
-                </div> 
-
-                <div class="card" style="width: 16rem; height: 8em; margin: 1em; background-image:url({{asset('table.png')}})">
-                    <div class="card-body">
-                        <h5 class="card-title">Table 5
-                            <span class="badge badge-success float-right" style="font-size:.55em;">Available</span>
-                        </h5>
-                        <p class="card-text">Status: </p>
-                        <p class="card-text">Amount: </p>
-                       </div>
-                </div>  
-
-                <div class="card" style="width: 16rem; height: 8em; margin: 1em; background-image:url({{asset('table.png')}})">
-                    <div class="card-body">
-                        <h5 class="card-title">Table 6
-                            <span class="badge badge-success float-right" style="font-size:.55em;">Available</span>
-                        </h5>
-                        <p class="card-text">Status: </p>
-                        <p class="card-text">Amount: </p>
-                        </div>
-                </div>  
-
-                <div class="card" style="width: 16rem; height: 8em; margin: 1em; margin-left: 5em; background-image:url({{asset('table.png')}})">
-                    <div class="card-body">
-                        <h5 class="card-title">Table 7 
-                            <span class="badge badge-success float-right" style="font-size:.55em;">Available</span>
-                        </h5>
-                        <p class="card-text">Status: </p>
-                        <p class="card-text">Amount: </p>
-                        </div>
-                </div>  
-
-                <div class="card" style="width: 16rem; height: 8em; margin: 1em; background-image:url({{asset('table.png')}})">
-                    <div class="card-body">
-                        <h5 class="card-title">Table 8
-                            <span class="badge badge-success float-right" style="font-size:.55em;">Available</span>
-                        </h5>
-                        <p class="card-text">Status: </p>
-                        <p class="card-text">Amount: </p>
-                        </div>
-                </div>  
-
-                <div class="card" style="width: 16rem; height: 8em; margin: 1em; background-image:url({{asset('table.png')}})">
-                    <div class="card-body">
-                        <h5 class="card-title">Table 9
-                            <span class="badge badge-success float-right" style="font-size:.55em;">Available</span>
-                        </h5>
-                        <p class="card-text">Status: </p>
-                        <p class="card-text">Amount: </p>
-                        </div>
-                </div>
-
-                <div class="card" style="width: 16rem; height: 8em; margin: 1em; margin-left: 5em; background-image:url({{asset('table.png')}})">
-                    <div class="card-body">
-                        <h5 class="card-title">Table 10
-                            <span class="badge badge-success float-right" style="font-size:.55em;">Available</span>
-                        </h5>
-                        <p class="card-text">Status: </p>
-                        <p class="card-text">Amount: </p>
-                        </div>
-                </div>  
-
-                <div class="card" style="width: 16rem; height: 8em; margin: 1em; background-image:url({{asset('table.png')}})">
-                    <div class="card-body">
-                        <h5 class="card-title">Table 11
-                            <span class="badge badge-success float-right" style="font-size:.55em;">Available</span>
-                        </h5>
-                        <p class="card-text">Status: </p>
-                        <p class="card-text">Amount: </p>
-                       </div>
-                </div> 
-
-                <div class="card" style="width: 16rem; height: 8em; margin: 1em; background-image:url({{asset('table.png')}})">
-                    <div class="card-body">
-                        <h5 class="card-title">Table 12
-                            <span class="badge badge-success float-right" style="font-size:.55em;">Available</span>
-                        </h5>
-                        <p class="card-text">Status: </p>
-                        <p class="card-text">Amount: </p>
-                     </div>
-                </div> 
+                @for($index = 1; $index <=12; $index++) 
+                <a data-toggle="modal" data-target="#exampleModal" style="cursor:pointer">
+                     
+                <div class="card mx-2" style="width:16rem; height:7.5em; background-image:url({{asset('table.png')}}); background-size:cover; background-repeat:no-repeat;">
+                        <div class="card-body">
+                            <h5 class="card-title">Table {{$index}}
+                                <span class="badge badge-success float-right" style="font-size:.55em;">Available</span>
+                            </h5>
+                            <p class="card-text">Status: </p>
+                            <p class="card-text">Amount: </p>
+                         </div>
+                    </div> 
+                </a>
+                @endfor 
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Orders of table #1</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+                <table class="table table-sm table-bordered" style="text-align:center">
+                    <tr>
+                        <th>Order</th>
+                        <th>Quantity</th>
+                        <th>Price</th>
+                    </tr>
+                    <tr>
+                       <td>Tapsilog</td>
+                       <td>3</td>
+                       <td>150</td> 
+                    </tr>
+                    <tr>
+                        <td>Sisig</td>
+                        <td>2</td>
+                        <td>250</td> 
+                         </tr>  
+                </table>
+                <a href="/make-order">
+                <button type="button" class="btn btn-primary" style="float:right; background-color:blue">Add new order</button> 
+                </a>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
 @endsection
