0cb49a41920a86bf8f0ec888c5faa433344d4883
jmcamille <2161284@slu.edu.ph>
2019-05-10 16:15:03 +0800

    Modified UI for makeOrder.blade.php

--- resources/views/pos/makeOrder.blade.php
+++ resources/views/pos/makeOrder.blade.php
@@ -9,7 +9,7 @@
 </a>
         <div class="container pb-5"> 
             <div class="row">
-                <div class="col-md-4 order-md-8 mb-4 mx-0">
+                <div class="col-md-4 order-md-8 mb-4 mx-0" style="margin-top: 2.5em;">
                     <div class="card p-0 mx-0">
                         <h4 class="text-muted" style="text-align:center; padding:0.5em;">Charges</h4>
                         <table class="table table-sm" style="font-size:.88em;">
@@ -56,48 +56,40 @@
                     </div>
                 </div>
         
-                <div class="col-md-8">
-                    <div class="nav nav-tabs" aria-orientation="vertical">
-                        <ul class="nav flex-column">
-                            <li class="nav-item">
-                                <a class="nav-link active" href="">Appetizer</a>
-                                <li class="nav-item">
-                                    <a class="nav-link" style="color:#505050"; href="">Bread</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" style="color:#505050"; href="">Breakfast</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" style="color:#505050"; href="">Group Meals</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" style="color:#505050"; href="">Noodles</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" style="color:#505050"; href="">Rice Bowl</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" style="color:#505050"; href=""> Soup</a>
-                                </li>
-                            </li>
-                        </ul>
-                        <div class="tab-content" id="">
-                            <div class="card" style="width: 7rem; height: 5em; margin-left: 1em;">
+                         
+                        <div class="row">
+                            <div class="col-md-14" style="margin-top: 3em;">
+                                <div class="list-group">
+                    
+                                    <a href="#" class='list-group-item active'>Appetizer</a>
+                                    <a href="#" class='list-group-item'>Bread</a>
+                                    <a href="#" class='list-group-item'>Breakfast</a>
+                                    <a href="#" class='list-group-item'>Group Meals</a>
+                                    <a href="#" class='list-group-item'>Noodles</a>
+                                    <a href="#" class='list-group-item'>Rice Bowl</a>
+                                    <a href="#" class='list-group-item'>Soup</a>
+                                    <a href="#" class='list-group-item'>Beverages </a>
+                                </div>
+                            </div>
+
+                    <div class="card" style="height:24.5em; width: 30em; margin-left: 1em; margin-top: 3em;"> 
+                        <div class="list-content" id="">
+                            <div class="card" style="width: 7rem; height: 5em;">
                                 <div class="card-body">
                                     <p class="card-title">Sizzling Pork Sisig</p>
                                 </div>
                             </div> 
-                            <div class="card" style="width: 7rem; height: 5em; margin-left: 1em;">
+                            <div class="card" style="width: 7rem; height: 5em; margin: 1em;">
                                 <div class="card-body">
                                     <p class="card-title">Sizzling Tuna</p>
                                 </div>
                             </div>
-                            <div class="card" style="width: 7rem; height: 5em; margin-left: 1em;">
+                            <div class="card" style="width: 7rem; height: 5em; margin:1em;">
                                 <div class="card-body">
                                     <p class="card-title">Nachos</p>
                                 </div>
                             </div>  
-                            <div class="card" style="width: 7rem; height: 5em; margin-left: 1em;">
+                            <div class="card" style="width: 7rem; height: 5em; margin: 1em;">
                                 <div class="card-body">
                                     <p class="card-title">Dynamite</p>
                                 </div>
