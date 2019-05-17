946a082ac63c1f0b448f91f185bdd6e42d6e9c78
Ian Culanag <2160301@slu.edu.ph>
2019-05-17 15:27:02 +0800

    Modify fb (cont)

--- app/Products.php
+++ app/Products.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Products extends Model
 {
-    //
+    //use softDeletes;
+
+    protected $dates = ['deleted_at'];
+
+    // Table Name
+    protected $table = 'products';
+    // Primary Key
+    public $primaryKey = 'id';
+    // Timestamps
+    public $timestamps = true;
+
+    protected $fillable = [
+        'foodCategory', 'foodName', 'price'
+    ];
 }
