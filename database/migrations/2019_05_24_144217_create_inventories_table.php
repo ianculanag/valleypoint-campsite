ceaa6278bd84722bf8009296adfc42bcf08940e6
Ian Culanag <2160301@slu.edu.ph>
2019-05-24 14:52:05 +0800

    Add inventories table

---
+++ database/migrations/2019_05_24_144217_create_inventories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInventoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inventories', function (Blueprint $table) {
+            $table->increments('id');            
+            $table->integer('ingredientID')->unsigned();     
+            $table->double('quantity', 8, 2);
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inventories');
+    }
+}
