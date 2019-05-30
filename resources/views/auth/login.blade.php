80630db28e9040950a62d6f64798af3f7526f17d
dawncundangan <2161487@slu.edu.ph>
2019-05-30 17:51:44 +0800

    Redesigned login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,44 +1,46 @@
 @extends('layouts.logapp')
 
 @section('content')
-        <div class="container">
-            <div class="py-5 text-center">
-                    <img src="{{asset('logo.jpg')}}" style="width: 12em;">
-                    <h2>Login to Valleypoint Campsite</h2>
-            </div>
+<style>
+    body {
+        background-image: url('valleypoint.png');
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+</style>
+<div class="container col-md-5 pt-5 mt-5">
+    <div class="card mt-5" style="background-color: #ffffffb3;">
+        <div class="mt-4 mb-3 text-center">
+            <img src="{{asset('logo.png')}}" style="width: 12em;">
+            {{-- <h2>Login to Valleypoint Campsite</h2> --}}
         </div>
-        <div class="container center-block">
-            <form method="POST" action="{{ route('login') }}" style="display: flex; flex-direction: column; align-items: center;">
+        <form method="POST" action="{{ route('login') }}" style="">
             @csrf
-
-                <div class="mb-3 col-md-4">
-                    <div class="form-group">
-                <input id="username" type="text" placeholder="Username" maxlength="25" class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}" name="username" value="{{ old('username') }}" required autofocus>
-                @if ($errors->has('username'))
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $errors->first('username') }}</strong>
-                </span>
-            @endif   
+            <div class="container px-5">
+                <div class="form-group my-2">
+                    <input id="username" type="text" placeholder="Username" maxlength="25" class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}" name="username" value="{{ old('username') }}" required autofocus>
+                    @if ($errors->has('username'))
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $errors->first('username') }}</strong>
+                    </span>
+                    @endif   
+                </div>
+                <div class="form-group my-2">
+                    <input id="password" type="password" placeholder="Password" maxlength="25" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                    @if ($errors->has('password'))
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $errors->first('password') }}</strong>
+                    </span>
+                    @endif
+                </div>
+                <button type="submit" class="btn btn-primary btn-block mt-4 mb-2">
+                    {{ __('Login') }}
+                </button>
+                <a href="#forgotPass" class="forgetPass">
+                    <p class="text-center pb-4">Forgot Password?</p>
+                </a>
             </div>
-
-                    <div class="form-group">
-                            <input id="password" type="password" placeholder="Password" maxlength="25" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required><br>
-                            @if ($errors->has('password'))
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $errors->first('password') }}</strong>
-                            </span>
-                        @endif
-                    </div>
-                    <div class="lower-card" style="display: flex; flex-direction: column; align-items: center;">
-                     <div class="form-group">
-                          <a href="#forgotPass" class="forgetPass"><p>Forgot Password?</p>
-                      </a></div>
-
-                      <button type="submit" class="btn btn-primary">
-                            {{ __('Login') }}
-                        </button>
-                    </div>
-                </div>
-                    </div>
-                </div>
+        </form>
+   </div> 
+</div>
 @endsection
